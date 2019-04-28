c724d66c76579376a4462c152ec6dc20488ece18
shivam222 <shivamb61@gmail.com>
2019-04-28 13:10:58 +0530

    check if error is of mongo atlas

--- server/routes/email.js
+++ server/routes/email.js
@@ -6,20 +6,21 @@ const router = express.Router();
 router.get('/exist/:email', (req, res) => {
     console.log('ddddddddddddddddddddddddd');
   const email= req.params.email.toLowerCase().trim();
-  User.find({
-    'email': email   //check if this email already exists
-}, function (err, emailData) {
-    if (err) {
-        res.send("error " + err);
-    } else {
-        if (emailData.length == 0) {
-            res.send(false);
-        }
-        else {
-            res.send(true);
-        }
-    }
-});
+  res.send(true);
+//   User.find({
+//     'email': email   //check if this email already exists
+// }, function (err, emailData) {
+//     if (err) {
+//         res.send("error " + err);
+//     } else {
+//         if (emailData.length == 0) {
+//             res.send(false);
+//         }
+//         else {
+//             res.send(true);
+//         }
+//     }
+// });
 });
 
 
