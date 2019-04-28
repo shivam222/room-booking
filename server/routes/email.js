9f419aa261e164326631093637ef3676e325012e
shivam222 <shivamb61@gmail.com>
2019-04-28 13:52:12 +0530

    correction

--- server/routes/email.js
+++ server/routes/email.js
@@ -6,21 +6,20 @@ const router = express.Router();
 router.get('/exist/:email', (req, res) => {
     console.log('ddddddddddddddddddddddddd');
   const email= req.params.email.toLowerCase().trim();
-  res.send(true);
-//   User.find({
-//     'email': email   //check if this email already exists
-// }, function (err, emailData) {
-//     if (err) {
-//         res.send("error " + err);
-//     } else {
-//         if (emailData.length == 0) {
-//             res.send(false);
-//         }
-//         else {
-//             res.send(true);
-//         }
-//     }
-// });
+  User.find({
+    'email': email   //check if this email already exists
+}, function (err, emailData) {
+    if (err) {
+        res.send("error " + err);
+    } else {
+        if (emailData.length == 0) {
+            res.send(false);
+        }
+        else {
+            res.send(true);
+        }
+    }
+});
 });
 
 
--- server/routes/org.js
+++ server/routes/org.js
@@ -4,21 +4,22 @@ const Org = require('../../models/orgStructure');
 const router = express.Router();
 
 router.get('/exist/:org', (req, res) => {
-  const org= req.params.org.toLowerCase().trim();
-  Org.find({
-    'name': org   //check if this org already exists
-}, function (err, orgData) {
-    if (err) {
-        res.send("error " + err);
-    } else {
-        if (orgData.length == 0) {
-            res.send(false);
-        }
-        else {
-            res.send(true);
-        }
-    }
-});
+    res.send(false);
+//   const org= req.params.org.toLowerCase().trim();
+//   Org.find({
+//     'name': org   //check if this org already exists
+// }, function (err, orgData) {
+//     if (err) {
+//         res.send("error " + err);
+//     } else {
+//         if (orgData.length == 0) {
+//             res.send(false);
+//         }
+//         else {
+//             res.send(true);
+//         }
+//     }
+// });
 });
 
 
